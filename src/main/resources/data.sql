59103335b45ca259d5e9db8b5d9c34fb0cc4dcc7
zarsakaev <z.arsakaev@bintech.kz>
2023-09-22 22:47:06 +0600

    todo list project

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
--- insert into user(id, username, password) values('1001','aaaa','{noop}pas');
--- insert into user(id, username, password) values('1002','bbbb','{noop}pass');
--- insert into user(id, username, password) values('1003','cccc','{noop}passs');
